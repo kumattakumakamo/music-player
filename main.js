d8faf15979998304cd616983c53fc01988090eb8
kumattakumakamo <fujie.kotokoto@gmail.com>
2026-02-09 06:24:06 +0900

    りせっとー

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
-const { app, BrowserWindow, session } = require('electron');
-const path = require('path');
-const http = require('http');
-const fs = require('fs');
-
-let server;
+const { app, BrowserWindow, screen } = require('electron');
 
 function createWindow() {
-    // 1. ローカルサーバーを起動 (ポート 3000)
-    server = http.createServer((req, res) => {
-        let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                res.end();
-                return;
-            }
-            res.writeHead(200);
-            res.end(data);
-        });
-    }).listen(3000);
 
     const win = new BrowserWindow({
-        width: 1000,
-        height: 800,
+        width: 300,
+        height: 100,
+        x: 0,
+        y: 0,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: true
+            nodeIntegration: true
         }
     });
-
-    // 2. YouTubeへの紹介状(Referer)をlocalhostにする
-    session.defaultSession.webRequest.onBeforeSendHeaders(
-        { urls: ['https://www.youtube.com/*', 'https://*.youtube.com/*'] },
-        (details, callback) => {
-            details.requestHeaders['Referer'] = 'http://localhost:3000/';
-            callback({ cancel: false, requestHeaders: details.requestHeaders });
-        }
-    );
-
-    // 3. 起動URLを http://localhost に変更
-    win.loadURL('http://localhost:3000/index.html');
+    win.setAlwaysOnTop(true, 'screen-saver');// さらに最前面に設定
+    win.loadFile('index.html');//index.htmlを読み込む
 }
 
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (server) server.close();
-    if (process.platform !== 'darwin') app.quit();
-});
+app.whenReady().then(createWindow);//アプリ起動時にウィンドウを作成
